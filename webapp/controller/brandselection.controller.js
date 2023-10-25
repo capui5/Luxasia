a37effe346530db527a597d084b41a57f8900b7a
Tejasri Bonala <tejasri.bonala@aarini.com>
2023-10-25 13:01:21 +0000

    second commit

--- webapp/controller/brandselection.controller.js
+++ webapp/controller/brandselection.controller.js
@@ -33,32 +33,36 @@ sap.ui.define([
       var name = this.getView().byId("nameInput").getValue();
       var surname = this.getView().byId("surnameInput").getValue();
 
-      var data = [
-        { mobile: "1234567890", email: "user1@example.com", name: "ABC", surname: "DEF" },
-        { mobile: "9876543210", email: "user2@example.com", name: "GHI", surname: "JKL" },
-      ];
+     
+      jQuery.ajax({
+        url: "../model/customerData.json",
+        dataType: "json",
+        success: function (data) {
+          var searchResults = data.filter(function (item) {
+            return (
+              (mobileNumber === "" || item.mobile.includes(mobileNumber)) &&
+              (emailAddress === "" || item.email.includes(emailAddress)) &&
+              (name === "" || item.name.includes(name)) &&
+              (surname === "" || item.surname.includes(surname))
+            );
+          });
 
-      var searchResults = data.filter(function (item) {
-        return (
-          (mobileNumber === "" || item.mobile.includes(mobileNumber)) &&
-          (emailAddress === "" || item.email.includes(emailAddress)) &&
-          (name === "" || item.name.includes(name)) &&
-          (surname === "" || item.surname.includes(surname))
-        );
+          if (searchResults.length > 0) {
+            MessageToast.show("Search results found!");
+          } else {
+            MessageToast.show("No results found.");
+
+            this.getView().byId("mobileInput").setValue("");
+            this.getView().byId("emailInput").setValue("");
+            this.getView().byId("nameInput").setValue("");
+            this.getView().byId("surnameInput").setValue("");
+          }
+        }.bind(this),
+        error: function () {
+          MessageToast.show("Error loading customer data.");
+        },
       });
-
-      if (searchResults.length > 0) {
-        MessageToast.show("Search results found!");
-      } else {
-        MessageToast.show("No results found.");
- 
-        this.getView().byId("mobileInput").setValue("");
-        this.getView().byId("emailInput").setValue("");
-        this.getView().byId("nameInput").setValue("");
-        this.getView().byId("surnameInput").setValue("");
-      }
     }
-
     //search//
   });
 });
--- webapp/controller/newcustomer.controller.js
+++ webapp/controller/newcustomer.controller.js
@@ -1,48 +1,77 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-   
-], function (Controller,MessageToast) {
-    "use strict";
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+  "use strict";
 
-    return Controller.extend("luxasia.controller.newcustomer", {
-        onInit: function () {},
-
-        onCreateProfile: function () {
-            var view = this.getView();
-      
+  return Controller.extend("luxasia.controller.newcustomer", {
+      onInit: function () {},
+      onCreateProfile: function () {
+        var view = this.getView();
+        var requiredFields = [
+            view.byId("titleComboBox"),
+            view.byId("nameInput"),
+            view.byId("surnameInput"),
+            view.byId("emailInput"),
+            view.byId("countryCodeComboBox"),
+            view.byId("mobileNumberInput"),
+            view.byId("dateOfBirthDatePicker"),
+            view.byId("customerAddressInput")
+        ];
+    
+        var allFieldsFilled = true;
+    
+        requiredFields.forEach(function (field) {
+            var value = field.getValue();
+            if (!value) {
+                field.setValueState("Error");
+                field.setValueStateText("This field is required");
+                allFieldsFilled = false;
+            } else {
+                field.setValueState("None");
+            }
+        });
+    
+        if (!allFieldsFilled) {
+            MessageToast.show("Please fill in all required fields.");
+            return;
+        }
+    
+        var newProfile = {
+            title: view.byId("titleComboBox").getSelectedKey(),
+            name: view.byId("nameInput").getValue(),
+            surname: view.byId("surnameInput").getValue(),
+            email: view.byId("emailInput").getValue(),
+            countryCode: view.byId("countryCodeComboBox").getSelectedKey(),
+            mobileNumber: view.byId("mobileNumberInput").getValue(),
+            dateOfBirth: view.byId("dateOfBirthDatePicker").getValue(),
+            customerAddress: view.byId("customerAddressInput").getValue()
+        };
+    
+       
+        $.getJSON("../model/customerData.json", function (data) {
            
-            var title = view.byId("titleInput").getValue();
-            var name = view.byId("nameInput").getValue();
-            var surname = view.byId("surnameInput").getValue();
-            var emailAddress = view.byId("emailInput").getValue();
-            var countryCode = view.byId("countryCodeComboBox").getSelectedKey();
-            var mobileNumber = view.byId("mobileNumberInput").getValue();
-            var dateOfBirth = view.byId("dateOfBirthInput").getValue();
-            var customerAddress = view.byId("customerAddressInput").getValue();
-      
-            var newProfile = {
-              title: title,
-              name: name,
-              surname: surname,
-              email: emailAddress,
-              countryCode: countryCode,
-              mobileNumber: mobileNumber,
-              dateOfBirth: dateOfBirth,
-              customerAddress: customerAddress,
-            };
-      
-            MessageToast.show("New profile created and data saved successfully!");
-      
-           
-            view.byId("titleInput").setValue("");
-            view.byId("nameInput").setValue("");
-            view.byId("surnameInput").setValue("");
-            view.byId("emailInput").setValue("");
-            view.byId("countryCodeComboBox").setSelectedKey("");
-            view.byId("mobileNumberInput").setValue("");
-            view.byId("dateOfBirthInput").setValue("");
-            view.byId("customerAddressInput").setValue("");
-          },
-    });
+            data.push(newProfile);
+    
+          
+            $.ajax({
+                url: "../model/customerData.json",
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function () {
+                    MessageToast.show("New profile created and data saved successfully!");
+    
+                    requiredFields.forEach(function (field) {
+                        field.setValue("");
+                    });
+                },
+                error: function () {
+                    MessageToast.show("Failed to update customer data.");
+                }
+            });
+        });
+    }
+    
+  });
 });
